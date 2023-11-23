dc4129aa15de98d3a21aac440c9f7b9d373af11b
LauraCascallana <lauracascallanaferreira@gmail.com>
2023-11-23 14:07:26 +0100

    programacion 23-24

--- Trimestre1/cuatro/ejemplos/cuadrado.java
+++ Trimestre1/cuatro/ejemplos/cuadrado.java
@@ -1,6 +1,14 @@
 package ejemplos;
 
 public class cuadrado {
-    
+    private int lado;
+
+    cuadrado(int lado){
+        this.lado=lado;
+    }
+
+    public int getArea(int lado){
+        return (lado*lado);
+    }
     
 }
---
+++ Trimestre1/cuatro/recursos/potencial.java
@@ -0,0 +1,25 @@
+package recursos;
+
+import java.util.Scanner;
+
+public class potencial {
+
+    public static int exponente(int base, int exponente){
+        int potencia=1;
+        for(int i=0; i<exponente; i++){
+            potencia*=base;
+        }
+        return potencia;
+    }
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dame la base: ");
+        int base=sc.nextInt();
+        System.out.println("Dame la potencia: ");
+        int exponente=sc.nextInt();
+
+        System.out.println("La potencia del número es: "+exponente(base, exponente));
+    }
+}
