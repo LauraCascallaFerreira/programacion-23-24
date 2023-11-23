cf3d3fd4b748d24deb8c95e6031b9ef3e4f0d31b
LauraCascallana <lauracascallanaferreira@gmail.com>
2023-11-23 10:55:17 +0100

    programacion 23-24

---
+++ Trimestre1/cuatro/ejemplos/cohete.java
@@ -0,0 +1,27 @@
+package ejemplos;
+
+public class cohete {
+
+    public static int numCohetes=0;
+
+    cohete(){
+        numCohetes++;
+    }
+
+    public int getCohetes(){
+        return numCohetes;
+    }
+
+    public static void setCohetes(int numCohetes){
+        cohete.numCohetes=numCohetes;
+    }
+
+    public static void main(String[] args) {
+        cohete c1 = new cohete();
+        cohete c2 = new cohete();
+        cohete c3 = new cohete();
+        System.out.println(c1.getCohetes());
+        System.out.println(c2.getCohetes());
+    }
+    
+}
