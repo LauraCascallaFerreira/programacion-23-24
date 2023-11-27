e28627ac3989ac9a3ff370e3bf5be62045f121c2
LauraCascallana <lauracascallanaferreira@gmail.com>
2023-11-27 14:41:08 +0100

    programacion 23-24

--- Trimestre1/cuatro/propuestos/piramide.java
+++ Trimestre1/cuatro/propuestos/piramide.java
@@ -8,9 +8,28 @@ public class piramide {
        1 2 1
       1 3 3 1
      1 4 6 4 1 */
+
+     public static int elemento(int fila, int columna){
+            if(columna==1)
+                return 1;
+            if(columna<1 || columna>fila)
+                return 0;
+            return elemento(fila-1, columna) + elemento (fila-1, columna-1);
+    }
+
     public static void main(String[] args) {
 
-        
+        int numFilas=5;
+        for(int i=1; i<(numFilas+1); i++){
+            for(int e=0; e<(numFilas-1); e++) 
+                System.out.print(" ");
+                for(int j=1; j<(numFilas+1); j++){
+                    int dato=elemento(i, j);
+                    if(dato>0)
+                        System.out.print(dato+" ");
+                }
+                System.out.println();
+        } //FIN EXTERIOR FILAS
         
     }
 }
