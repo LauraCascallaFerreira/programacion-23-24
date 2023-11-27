13010f9e4ea7b55ab1e3a482b48698b27af552aa
LauraCascallana <lauracascallanaferreira@gmail.com>
2023-11-27 14:29:18 +0100

    programacion 23-24

---
+++ Trimestre1/cuatro/propuestos/piramide.java
@@ -0,0 +1,16 @@
+package propuestos;
+
+public class piramide {
+
+    //Mostrar esta piramide por recursividad
+    /*   1
+        1 1
+       1 2 1
+      1 3 3 1
+     1 4 6 4 1 */
+    public static void main(String[] args) {
+
+        
+        
+    }
+}
